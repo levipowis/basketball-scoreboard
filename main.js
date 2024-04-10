51450e4262e7d1f722cfb89a65583478d558807c
Levi Powis <lpowis97@gmail.com>
2024-04-10 16:51:19 -0400

    Add variables to store homeScore and guestScore elements

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let homeScore = 0;
 let guestScore = 0;
 
 // Create variables to store the home-score and guest-score elements
+let homeScoreEl = document.getElementById("home-score");
+let guestScoreEl = document.getElementById("guest-score");
 
 // Set textContent for home-score and guest-score to the homeScore and guestScore
